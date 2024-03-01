9d7b2f05ed941837ae0d1b6b8ab713c1031fe235
Rafael Luz <rafaelmelo.aluno@unipampa.edu.br>
2024-03-01 10:26:32 -0300

    teste passando path pelo import

--- src/components/navbarVictus.tsx
+++ src/components/navbarVictus.tsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
+import logo from '../assets/images/VICTUS1b.png'; // Ajuste o caminho conforme necessário
+import qrCode from '../assets/images/formQrCode.svg'; // Ajuste o caminho conforme necessário
+
 
 export function NavbarVicuts() {
   return (
@@ -13,7 +16,7 @@ export function NavbarVicuts() {
             <Link className='p-3' to='/'>
             <img
               alt=""
-              src='/src/assets/images/VICTUS1b.png'
+              src={logo}
               width="300"
               height="300"
               className='align-top'
@@ -24,7 +27,7 @@ export function NavbarVicuts() {
           <Link className='p-3 flex flex-row items-center justify-center' to='https://forms.gle/Z3hnWQDT6F3o1Vi76' target='blank_' rel='noopener noreferrer'>
           <p className='text-center align-middle text-lg font-medium'>Avalie o Victus Charts:</p>
             <img
-             src="/src/assets/images/formQrCode.svg"
+             src={qrCode}
               width="100"
               height="100"
               className='align-top'
