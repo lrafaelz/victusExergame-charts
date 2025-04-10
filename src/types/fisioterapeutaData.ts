1817c930d72c231c4e7345117416821847644f3f
Rafael Luz <rafaelmelo.aluno@unipampa.edu.br>
2025-04-10 01:18:45 -0300

    feat: Add Fisioterapeuta interface and extend Patient interface with new properties; introduce PacienteSession for session data management

---
+++ src/types/fisioterapeutaData.ts
@@ -0,0 +1,8 @@
+export interface Fisioterapeuta {
+  email: string;
+  loginHistory: {
+    0: string; // timestamp ISO
+  };
+  nome: string;
+  uid: string;
+}
--- src/types/patientData.ts
+++ src/types/patientData.ts
@@ -1,5 +1,19 @@
 export interface Patient {
-  name: string;
-  age: number;
-  description?: string;
+  id?: string;
+  nome?: string;
+  idade?: number;
+}
+
+export interface PacienteSession {
+  BPM: {
+    [key: number]: number;
+  };
+  EMG: {
+    [key: number]: number;
+  };
+  distancia: number;
+  pontuacao: number;
+  velocidade: {
+    [key: number]: number;
+  };
 }
